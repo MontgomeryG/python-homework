71850cf2c08dfb0dee9cc9d4f360860b0767ac54
montygash <montygash@gmail.com>
2023-10-31 10:52:57 -0700

    minor formatting changes

--- Module 7 Challenge/Data/credit_card_schema.sql
+++ Module 7 Challenge/Data/credit_card_schema.sql
@@ -1,3 +1,6 @@
+--HINT** "For the credit_card and transaction tables, the card column should be a VARCHAR (20) datatype rather than an INT."
+--card, cardholder_id
+
 CREATE TABLE credit_card_table (
 card VARCHAR(20) NOT NULL,
 cardholder_id INT NOT NULL
--- Module 7 Challenge/Data/transaction_schema.sql
+++ Module 7 Challenge/Data/transaction_schema.sql
@@ -1,4 +1,7 @@
 -- Create table for transactions
+-- HINT** "For the transaction table, the date column should be a TIMESTAMP datatype rather than DATE."
+--HINT** "For the credit_card and transaction tables, the card column should be a VARCHAR (20) datatype rather than an INT."
+
 --id,date,amount,card,id_merchant
 CREATE TABLE transaction_table(
   id VARCHAR(30) NOT NULL,
